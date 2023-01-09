db5c6b0966807fb6e4c62e93571e922108c564d5
Max Oliveira <max-oliveira@outlook.com.br>
2023-01-09 14:05:51 -0300

    feat: create migrations

--- database/migrations/2023_01_09_165156_create_work_card_table.php
+++ database/migrations/2023_01_09_165156_create_work_card_table.php
@@ -13,15 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('work_card', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('cpf')->unique();
-            $table->boolean('admin')->default(0);
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('pis_pasep')->unique();
+            $table->string('code');
+            $table->string('serie');
+            $table->string('UF');
             $table->timestamps();
         });
     }
@@ -33,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('work_card');
     }
 };
